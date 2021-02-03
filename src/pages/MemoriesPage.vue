5f8fdd14aaaa32c431ca24385edf91ac7a66c128
Fabrizio Azzarri <fabrizio.azzarri@gmail.com>
2021-02-03 14:55:32 +0100

    memories components refactored

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,34 +1,21 @@
 <template>
     <base-layout page-title="All memories">
-        <ion-list>
-            <ion-item v-for="memory in memories" :key="memory.id" :router-link="`/memories/${memory.id}`">
-                <ion-thumbnail slot="start">
-                    <ion-img :src="memory.img" :alt="memory.title"></ion-img>
-                </ion-thumbnail>
-                <ion-label>
-                    {{ memory.title }}
-                </ion-label>
-            </ion-item>
-        </ion-list>
+        <memories-list :memories="memories"></memories-list>
     </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem, IonImg, IonThumbnail, IonLabel } from '@ionic/vue';
+import MemoriesList from '../components/memories/MemoriesList.vue';
 
 export default {
     components: {
-        IonList,
-        IonItem,
-        IonImg,
-        IonThumbnail,
-        IonLabel
+        MemoriesList
     },
     computed: {
         memories() {
             return this.$store.getters.memories;
         }
-    }
+    },
 };
 </script>
 
